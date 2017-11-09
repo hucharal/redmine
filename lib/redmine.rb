0c661ce82cdb1212ead958ca090417c5fb32d70b
Hugo Amaral <hucharal13@gmail.com>
2017-11-09 14:37:53 -0200

    Create new permission for manage issue times

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -112,6 +112,7 @@ Redmine::AccessControl.map do |map|
     map.permission :view_private_notes, {}, :read => true, :require => :member
     map.permission :set_notes_private, {}, :require => :member
     map.permission :delete_issues, {:issues => :destroy}, :require => :member
+    map.permission :manage_issue_times, {}
     # Watchers
     map.permission :view_issue_watchers, {}, :read => true
     map.permission :add_issue_watchers, {:watchers => [:new, :create, :append, :autocomplete_for_user]}
